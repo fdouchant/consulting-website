09bc26307059134b17c8ca75c120e5fa97ce49fa
Fabrice Douchant <fabrice.douchant@gmail.com>
2023-07-27 16:02:15 +0200

    refine link in the footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,3 @@
-<script setup>
-</script>
-
 <template>
   <full-page ref="fullpage" :options="options">
     <div class="section ">
@@ -22,11 +19,40 @@
   </full-page>
   <footer class="position-absolute bottom-0 end-0">
     <div class="d-flex justify-content-end p-2">
-      <button type="button" class="btn btn-primary btn-lg btn-rounded mx-1"><i class="bi bi-linkedin"></i></button>
-      <button type="button" class="btn btn-primary btn-lg btn-rounded mx-1"><i class="bi bi-envelope-fill"></i></button>
+      <button type="button" class="btn btn-primary btn-lg btn-footer-link mx-1"><i class="bi bi-linkedin"></i></button>
+      <button type="button" class="btn btn-primary btn-lg btn-footer-link mx-1"><i class="bi bi-envelope-fill"></i></button>
     </div>
-  </footer>
+    </footer>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        navigation: true,
+        credits: { // TODO add a notice about fullPage.js
+          enabled: false,
+          label: '',
+        }
+      }
+    }
+  }
+}
+</script>
+
 <style scoped>
+.btn-footer-link {
+  position: relative;
+  border-radius: 50%;
+  height: 50px;
+  width: 50px;
+
+  > i {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
 </style>
