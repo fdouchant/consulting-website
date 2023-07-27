cb8a8c27986f33fd4a11183a11c544a36588fb6f
Fabrice Douchant <fabrice.douchant@gmail.com>
2023-07-27 16:28:33 +0200

    add new section for prestation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <full-page ref="fullpage" :options="options">
-    <div class="section ">
-    <div class="text-center">
-      <h1>Fabrice Douchant Consulting</h1>
-      <img src="https://placehold.co/300x200" class="my-3" alt="Fabrice Douchant Consulting Logo">
-      <h2>Expert en projet digital</h2>
-      <button class="btn btn-primary my-4" @click="$refs.fullpage.api.moveSectionDown()">
-        En savoir plus
-      </button>
+    <div class="section text-center">
+      <div class="text-center">
+        <h1>Fabrice Douchant Consulting</h1>
+        <img src="https://placehold.co/300x200" class="my-3" alt="Fabrice Douchant Consulting Logo">
+        <h2>Expert en projet digital</h2>
+        <button class="btn btn-primary my-4" @click="$refs.fullpage.api.moveSectionDown()">
+          En savoir plus
+        </button>
       </div>
     </div>
     <div class="section">
-      <button class="prev" @click="$refs.fullpage.api.moveSectionUp()">
-        Prev
-      </button>
-      Section 2
+      <div class="px-5">
+        <h1 class="text-center mb-5">Présentation</h1>
+        <div class="card flex-row border-0">
+          <img src="https://placehold.co/100x100" alt="Fabrice Douchant" class="card-img-left align-self-start rounded">
+          <div class="card-body py-0">
+            <p>Issu d'une formation universitaire en informatique, j'ai évolué dans plusieurs rôles techniques et de
+              gestion.
+              Fort de cette expérience complète et reconnue dans les technologies de l'information, je me spécialise dans
+              la
+              transformation digitale : la modernisation d'un système ou la dématérialisation d'un processus métier dans
+              le
+              cadre de projets transverses à forte valeur ajoutée.</p>
+            <p>Mes domaines d'intervention sont multiples : finances et assurances, institutions publiques, communication
+              et
+              événementiel, sociétés de services, éditeurs de solutions.</p>
+
+            <figure class="text-center">
+              <blockquote class="blockquote">
+                <p>Un projet est une aventure humaine qui requière coopération, rigueur, pragmatisme et une
+                  bonne dose d'optimisme</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">Ma devise</figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
     </div>
   </full-page>
   <footer class="position-absolute bottom-0 end-0">
     <div class="d-flex justify-content-end p-2">
       <button type="button" class="btn btn-primary btn-lg btn-footer-link mx-1"><i class="bi bi-linkedin"></i></button>
-      <button type="button" class="btn btn-primary btn-lg btn-footer-link mx-1"><i class="bi bi-envelope-fill"></i></button>
+      <button type="button" class="btn btn-primary btn-lg btn-footer-link mx-1"><i
+          class="bi bi-envelope-fill"></i></button>
     </div>
-    </footer>
+  </footer>
 </template>
 
 <script>
@@ -32,7 +55,7 @@ export default {
       options: {
         navigation: true,
         credits: { // TODO add a notice about fullPage.js
-          enabled: false,
+          enabled: false, // not working
           label: '',
         }
       }
@@ -48,11 +71,10 @@ export default {
   height: 50px;
   width: 50px;
 
-  > i {
+  >i {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
   }
-}
-</style>
+}</style>
